18a0f412abecdb18ee48b99181ef7db9097c7c8a
Whitney Murray <wnksmith92@yahoo.com>
2015-12-30 18:50:40 -0600

    I think I have it working but little pizza server isn't working

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -2,34 +2,41 @@
 'use strict';
 $(document).ready(function () {
 	var menu = $('#menu');
-	var pic = $('pic');
-	var url = $('url');
-	var cap = $('cap');
-	var selfie = $('selfie');
-	var display = $('display');
-	var cancel = $('cancel');
+	var pic = $('#pic');
+	var url = $('#url');
+	var cap = $('#cap');
+	var selfie = $('#selfie');
+	var display = $('#display');
+	var cancel = $('#cancel');
 
-	$.get('http://tiyfe.herokuapp.com/deathtoselfie', function (response) {
+	$.get('http://tiyfe.herokuapp.com/collections/deathtoselfie', function (response) {
 		response.forEach(function (response, index) {
-			$display.append('<div class = "photoBox"><div><img class = "newPhoto"s src= ' + response.photo + '></img></div><div class ="newCap"> ' + response.caption + '</div></div>');
+			display.append('<div class = "photoBox"><div><img class = "newPhoto"s src= ' + response.photo + '></img></div><div class ="newCap"> ' + response.caption + '</div></div>');
 		});
 	}, 'json');
-	$menu.click(function () {
-		$pic.slideToggle('slow');
-		var newUrl = $url.val(' ');
-		var newCap = $cap.val(' ');
+	menu.click(function (e) {
+		e.preventDefault();
+		pic.slideToggle('slow');
+		var newUrl = url.val();
+		var newCap = cap.val();
 	});
 
-	$.post('http://tiyfe.herokuapp.com/deathtoselfie', {
-		photo: newUrl,
-		caption: newCaption
-	}, function (response) {
-		$display.prepend('<div class = "photoBox"><div><img class = "newPhoto"s src= ' + response.photo + '></img></div><div class ="newCap"> ' + response.caption + '</div></div>');
-	}, 'json');
-	$pic.slideToggle('fast');
-	$cancel.click(function () {
-		$url.val(' ');
-		$cap.val(' ');
+	selfie.submit(function (e) {
+		e.preventDefault();
+		var newUrl = url.val();
+		var newCaption = cap.val();
+
+		$.post('http://tiyfe.herokuapp.com/deathtoselfie', {
+			photo: newUrl,
+			caption: newCaption
+		}, function (response) {
+			display.prepend('<div class = "photoBox"><div><img class = "newPhoto" src= ' + response.photo + '></img></div><div class ="newCap"> ' + response.caption + '</div></div>');
+		}, 'json');
+		pic.slideToggle('fast');
+	});
+	cancel.click(function () {
+		url.val();
+		cap.val();
 	});
 });
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,34 @@
 'use strict';
 $(document).ready(function(){
 	var menu = $('#menu');
-	var pic = $('pic');
-	var url = $('url');
-	var cap = $('cap');
-	var selfie = $('selfie');
-	var display = $('display');
-	var cancel = $('cancel');
+	var pic = $('#pic');
+	var url = $('#url');
+	var cap = $('#cap');
+	var selfie = $('#selfie');
+	var display = $('#display');
+	var cancel = $('#cancel');
 
 $.get(
-	'http://tiyfe.herokuapp.com/deathtoselfie',
+	'http://tiyfe.herokuapp.com/collections/deathtoselfie',
 	function(response){
 		response.forEach(function(response, index){
-			$display.append('<div class = "photoBox"><div><img class = "newPhoto"s src= '+ response.photo+'></img></div><div class ="newCap"> '+response.caption+'</div></div>')
+			display.append('<div class = "photoBox"><div><img class = "newPhoto"s src= '+ response.photo+'></img></div><div class ="newCap"> '+response.caption+'</div></div>')
 		})
 	},
 	'json'
 );
-$menu.click(function(){
-	$pic.slideToggle('slow');
-	var newUrl = $url.val(' ');
-	var newCap =$cap.val(' ');
+menu.click(function(e){
+	e.preventDefault();
+	pic.slideToggle('slow');
+	var newUrl = url.val();
+	var newCap =cap.val();
 });
+
+selfie.submit(function(e){
+	e.preventDefault();
+	var newUrl = url.val();
+	var newCaption = cap.val();
+
 
 	$.post(
 		'http://tiyfe.herokuapp.com/deathtoselfie',
@@ -30,14 +37,16 @@ $menu.click(function(){
 			caption: newCaption
 		},
 		function(response){
-			$display.prepend('<div class = "photoBox"><div><img class = "newPhoto"s src= '+ response.photo+'></img></div><div class ="newCap"> '+response.caption+'</div></div>')
+			display.prepend('<div class = "photoBox"><div><img class = "newPhoto" src= '+ response.photo+'></img></div><div class ="newCap"> '+response.caption+'</div></div>')
 		},
 		'json'
 		)
-	$pic.slideToggle('fast');
-	$cancel.click(function(){
-		$url.val(' ');
-		$cap.val(' ');
+	pic.slideToggle('fast');
+});
+	cancel.click(function(){
+		url.val();
+		cap.val();
+
 	});
 });
 
